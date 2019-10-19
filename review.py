291073f3644d78ac199819407700e3e2b604873e
Jeff Goh <gohchinhong00@gmail.com>
2019-10-19 13:36:48 +0800

    averange_data

--- review.py
+++ review.py
@@ -6,8 +6,10 @@ with open ("reviews.txt", "r") as f:
 		count += 1 #count = count + 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print("Data has been read,it have", len(data) ,"data") 
 
-print(data[0])
-print("----------------")
-print(data[1])
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print("Averange reviews is", sum_len/len(data))
