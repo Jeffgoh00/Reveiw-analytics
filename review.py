d5fe2e62a602d8c03105a3c7e3f0cd8d4b3ea44f
Jeff Goh <gohchinhong00@gmail.com>
2019-10-19 13:59:01 +0800

    add filter good

--- review.py
+++ review.py
@@ -15,7 +15,7 @@ print("Averange reviews is", sum_len/len(data))
 
 
 
-#筛选
+#筛选有多少留言少过100个字
 new = []
 for d in data:
 	if len(d) < 100:
@@ -24,3 +24,15 @@ print("Total have", len(new), "less than 100 words inside reviews")
 print(new[0])
 print("-------------------------------------------")
 print(new[1])
+
+#筛选有提到Good的留言
+good = []
+for d in data:
+	if "good" in d: #It will be a question at here, and the answer only with True/False
+		good.append(d)
+# "a" in "abc" -> True
+print("Total have", len(good), "mentioned good")
+print(good[0])
+
+
+
