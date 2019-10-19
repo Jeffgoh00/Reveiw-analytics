a589e4197c31722ac4ea4e1330062ea7e361601c
Jeff Goh <gohchinhong00@gmail.com>
2019-10-19 13:46:59 +0800

    read_filter

--- review.py
+++ review.py
@@ -8,11 +8,12 @@ with open ("reviews.txt", "r") as f:
 			print(len(data))
 print("Data has been read,it have", len(data) ,"data") 
 
-
 sum_len = 0
 for d in data:
 	sum_len += len(d)
 print("Averange reviews is", sum_len/len(data))
+
+
 
 #筛选
 new = []
@@ -20,3 +21,6 @@ for d in data:
 	if len(d) < 100:
 		new.append(d)
 print("Total have", len(new), "less than 100 words inside reviews")
+print(new[0])
+print("-------------------------------------------")
+print(new[1])
